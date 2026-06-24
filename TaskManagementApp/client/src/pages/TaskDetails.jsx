8afbe5e83306dc81f99d18cdc75c906fd04f698f
Thisal Gonsalkorala <tdimith10@gmail.com>
2026-06-24 11:39:46 +0530

    2nd edition init

--- TaskManagementApp/client/src/pages/TaskDetails.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TaskDetails = () => {
-  return <div></div>;
-};
-
-export default TaskDetails;
--- TaskManagementApp/client/src/pages/Trash.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Trash = () => {
-  return <div></div>;
-};
-
-export default Trash;
